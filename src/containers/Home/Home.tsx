1a55f596c3d43d0c256fc8b207dc5deae18c5246
Steve Hamm <steve@cygnalabs.com>
2020-11-11 12:14:17 -0400

    - Added react-reveal for fade effect. - Added left padding to list items. - Added Explorer text to home screen. - Decreased letter spacing on site title. - Added description div to home screen.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Fade } from "react-awesome-reveal";
 import Logo from '../../assets/images/starWarsLogo.png';
+import ExplorerText from '../../assets/images/explorer.png'
 import './Home.css';
 
 const Home = () => (
-  <div className="content">
-    <img src={Logo} alt="starWars" className="star-wars-logo" />
-  </div>
+  <Fade>
+    <div className="content">
+      <div className="content-inner">
+        <div className="content-left">
+          <img src={Logo} alt="starWars" className="star-wars-logo" />
+          <img src={ExplorerText} alt="explorer" className="explorer-text" />
+        </div>    
+        <div className="content-right">
+          <span className="description-top">The Star Wars Explorer is a simple application for viewing information on your favorite Star Wars movies, characters, and planets.</span>
+          <span className="description-bottom">Click the buttons above to view details.</span>
+        </div>
+      </div>
+    </div>
+  </Fade>
 );
 
 export default Home;
